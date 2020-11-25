d00ac5c4c82d8967a64803da7815b3090c0d563d
Sergio Maldonado <38842204+sergiomr04@users.noreply.github.com>
2020-11-25 14:56:18 -0600

    Create reto2.sql

---
+++ Sesion_7/reto2.sql
@@ -0,0 +1,14 @@
+-- Reto 2
+-- Sergio MAldonado Rodriguez
+-- Usando como base el archivo movies.dat, limpiarlo e importar los datos en la tabla movies creada en el Reto 1.
+-- Usando como base el archivo ratings.dat, limpiarlo e importar los datos en la tabla ratings creada en el Reto 2.
+
+
+USE bedu_test_2;
+SELECT *
+FROM movies
+LIMIT 10;
+
+SELECT *
+FROM ratings
+LIMIT 10;
