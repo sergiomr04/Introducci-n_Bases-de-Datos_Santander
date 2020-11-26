918b359bc481dbd68a67d84f454e6d200057baca
Sergio Maldonado <38842204+sergiomr04@users.noreply.github.com>
2020-11-25 22:13:50 -0600

    Create reto1.sql

---
+++ Sesion_1/reto1.sql
@@ -0,0 +1,9 @@
+-- Reto 1
+-- Sergio Maldonado Rodriguez
+-- Usando la base de datos tienda, muestra la descripción de las tablas articulo, puesto y venta. 
+-- Por cada tipo de dato que encuentras llena la siguiente tabla, a mano. Usa la Documentación de MySQL como referencia.
+USE tienda;
+DESCRIBE articulo;
+DESCRIBE puesto;
+DESCRIBE venta;
+
